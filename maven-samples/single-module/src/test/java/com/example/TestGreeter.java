ce586837b1fa3c025093747d50d2481be593bdc1
Shubham Patil <patilss16.it@coep.ac.in>
2020-10-15 14:55:26 +0530

    Added test cases

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -39,7 +39,7 @@ public class TestGreeter {
   }
 
     @Test
-  public void greetShouldIncludeGreetingMessage() {
+  public void greetShouldIncludeGreetingMessage1() {
     // String someone = "World, Thanks you making me Happy";
 
     // assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
